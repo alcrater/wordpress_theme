424c526eda0ad0cb4d70befe697c8dd3ba564139
Audrey <pacjc@msn.com>
2020-01-22 20:10:14 -0500

    404 comment

--- 404.php
+++ 404.php
@@ -15,7 +15,7 @@ get_header();
 
 			<section class="error-404 not-found">
 				<header class="page-header">
-					<h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'uppercase' ); ?></h1>
+					<h1 class="page-title"><?php esc_html_e( 'Be willing to let go of what you think your outcome is, to end up where you are supposed to be.', 'uppercase' ); ?></h1>
 				</header><!-- .page-header -->
 
 				<div class="page-content">
