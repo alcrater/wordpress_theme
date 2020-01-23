651957ee6edc3aa600f271ab3d5301ecd0e094a5
Audrey <pacjc@msn.com>
2020-01-22 21:58:17 -0500

    removed bottom section

--- 404.php
+++ 404.php
@@ -30,17 +30,9 @@ get_header();
 
 					the_widget( 'WP_Widget_Recent_Posts' );
 					?>
+	
 
 					
-
-					<?php
-					/* translators: %1$s: smiley */
-					$uppercase_archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', 'uppercase' ), convert_smilies( ':)' ) ) . '</p>';
-					the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$uppercase_archive_content" );
-
-					the_widget( 'WP_Widget_Tag_Cloud' );
-					?>
-
 				</div><!-- .page-content -->
 			</section><!-- .error-404 -->
 
