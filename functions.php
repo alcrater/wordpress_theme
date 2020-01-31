1cbfe7ca89ad66643dd5ce630c3db2da6f3bef44
Audrey <pacjc@msn.com>
2020-01-31 08:45:21 -0500

    change theme name

--- functions.php
+++ functions.php
@@ -4,10 +4,10 @@
  *
  * @link https://developer.wordpress.org/themes/basics/theme-functions/
  *
- * @package uppercase
+ * @package justpractice
  */
 
-if ( ! function_exists( 'uppercase_setup' ) ) :
+if ( ! function_exists( 'justpractice_setup' ) ) :
 	/**
 	 * Sets up theme defaults and registers support for various WordPress features.
 	 *
@@ -15,14 +15,14 @@ if ( ! function_exists( 'uppercase_setup' ) ) :
 	 * runs before the init hook. The init hook is too late for some features, such
 	 * as indicating support for post thumbnails.
 	 */
-	function uppercase_setup() {
+	function justpractice_setup() {
 		/*
 		 * Make theme available for translation.
 		 * Translations can be filed in the /languages/ directory.
 		 * If you're building a theme based on uppercase, use a find and replace
 		 * to change 'uppercase' to the name of your theme in all the template files.
 		 */
-		load_theme_textdomain( 'uppercase', get_template_directory() . '/languages' );
+		load_theme_textdomain( 'justpractice', get_template_directory() . '/languages' );
 
 		// Add default posts and comments RSS feed links to head.
 		add_theme_support( 'automatic-feed-links' );
@@ -44,7 +44,7 @@ if ( ! function_exists( 'uppercase_setup' ) ) :
 
 		// This theme uses wp_nav_menu() in one location.
 		register_nav_menus( array(
-			'menu-1' => esc_html__( 'Primary', 'uppercase' ),
+			'menu-1' => esc_html__( 'Primary', 'justpractice' ),
 		) );
 
 		/*
@@ -60,7 +60,7 @@ if ( ! function_exists( 'uppercase_setup' ) ) :
 		) );
 
 		// Set up the WordPress core custom background feature.
-		add_theme_support( 'custom-background', apply_filters( 'uppercase_custom_background_args', array(
+		add_theme_support( 'custom-background', apply_filters( 'justpractice_custom_background_args', array(
 			'default-color' => 'ffffff',
 			'default-image' => '',
 		) ) );
@@ -112,7 +112,7 @@ if ( ! function_exists( 'uppercase_setup' ) ) :
 		) );
 	}
 endif;
-add_action( 'after_setup_theme', 'uppercase_setup' );
+add_action( 'after_setup_theme', 'justpractice_setup' );
 
 /**
  * Set the content width in pixels, based on the theme's design and stylesheet.
@@ -121,47 +121,47 @@ add_action( 'after_setup_theme', 'uppercase_setup' );
  *
  * @global int $content_width
  */
-function uppercase_content_width() {
+function justpractice_content_width() {
 	// This variable is intended to be overruled from themes.
 	// Open WPCS issue: {@link https://github.com/WordPress-Coding-Standards/WordPress-Coding-Standards/issues/1043}.
 	// phpcs:ignore WordPress.NamingConventions.PrefixAllGlobals.NonPrefixedVariableFound
-	$GLOBALS['content_width'] = apply_filters( 'uppercase_content_width', 640 );
+	$GLOBALS['content_width'] = apply_filters( 'justpractice_content_width', 640 );
 }
-add_action( 'after_setup_theme', 'uppercase_content_width', 0 );
+add_action( 'after_setup_theme', 'justpractice_content_width', 0 );
 
 /**
  * Register widget area.
  *
  * @link https://developer.wordpress.org/themes/functionality/sidebars/#registering-a-sidebar
  */
-function uppercase_widgets_init() {
+function justpractice_widgets_init() {
 	register_sidebar( array(
-		'name'          => esc_html__( 'Sidebar', 'uppercase' ),
+		'name'          => esc_html__( 'Sidebar', 'justpractice' ),
 		'id'            => 'sidebar-1',
-		'description'   => esc_html__( 'Add widgets here.', 'uppercase' ),
+		'description'   => esc_html__( 'Add widgets here.', 'justpractice' ),
 		'before_widget' => '<section id="%1$s" class="widget %2$s">',
 		'after_widget'  => '</section>',
 		'before_title'  => '<h2 class="widget-title">',
 		'after_title'   => '</h2>',
 	) );
 }
-add_action( 'widgets_init', 'uppercase_widgets_init' );
+add_action( 'widgets_init', 'justpractice_widgets_init' );
 
 /**
  * Enqueue scripts and styles.
  */
-function uppercase_scripts() {
-	wp_enqueue_style( 'uppercase-style', get_stylesheet_uri() );
+function justpractice_scripts() {
+	wp_enqueue_style( 'justpractice-style', get_stylesheet_uri() );
 
-	wp_enqueue_script( 'uppercase-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
+	wp_enqueue_script( 'justpractice-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
 
-	wp_enqueue_script( 'uppercase-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
+	wp_enqueue_script( 'justpractice-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
 	}
 }
-add_action( 'wp_enqueue_scripts', 'uppercase_scripts' );
+add_action( 'wp_enqueue_scripts', 'justpractice_scripts' );
 
 /**
  * Implement the Custom Header feature.
